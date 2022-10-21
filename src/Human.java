18057637dd520f862eb6dba3149d3dd583161f4f
OlViks <salova.40@yandex.ru>
2022-10-21 16:46:07 +0300

    На проверку

--- src/Human.java
+++ src/Human.java
@@ -1,21 +1,35 @@
-import java.sql.Struct;
+import java.util.Objects;
 
 public class Human {
 
     int age;
-    String name;
-    String city;
+    final String name;
+    final String city;
+    String jobTitle;
 
+    Human(int age, String name, String city, String jobTitle) {
+        this.age = Math.max(age, 0);
+        this.name = Objects.requireNonNullElse(name, "Информация не указана.");
+        this.city = Objects.requireNonNullElse(city, "Информация не указана.");
+        this.jobTitle = Objects.requireNonNullElse(jobTitle,"Информация не указана.");
+    }
     Human(int age, String name, String city) {
-        this.age = age;
-        this.name = name;
-        this.city = city;
-
+        this.age = Math.max(age, 0);
+        this.name = Objects.requireNonNullElse(name, "Информация не указана.");
+        this.city = Objects.requireNonNullElse(city, "Информация не указана.");
     }
 
     void human() {
-        System.out.println(name + "из города" + city + " , которому"
-                + age + " лет");
+        System.out.println("Привет! Меня зовут " + name + " Я из города" + city + " , Я родился в "
+                + (2022 - age) + " году. Будем знакомы!");
 
     }
+
+    void helloHuman() {
+        System.out.println("Привет! Меня зовут " + name
+                + ". Я из города "+ city + ". Я родился в " + (2022 - age) + " году."
+                + " Я работаю на должности "+ jobTitle + ". Будем знакомы!");
+
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,59 @@
 public class Main {
     public static void main(String[] args) {
-        Human maksim = new Human(35, " Максим", " Минск");
-        maksim.human();
+        talk_1();
+        talk_2();
+        talk_3();
+        talk_4();
+
     }
+
+
+    private static void talk_1() {
+        Human maksim = new Human(35, " Максим", " Минск");
+        Human ana = new Human(-29, " Аня", " Москва");
+        Human kate = new Human(28, null, " Калининград");
+        Human artem = new Human(35, " Артем", " Москва");
+
+        maksim.human();
+        ana.human();
+        kate.human();
+        artem.human();
+        System.out.println();
+    }
+
+    private static void talk_2() {
+        Human maksim = new Human(35, " Максим", " Минск", "бренд-менеджером");
+        Human ana = new Human(29, " Аня", " Москва", "методистом образовательных программ");
+        Human kate = new Human(28, " Катя", " Калининград","продакт-менеджером");
+        Human artem = new Human(35, " Артем", " Москва","директором по развитию бизнеса");
+        maksim.helloHuman();
+        ana.helloHuman();
+        kate.helloHuman();
+        artem.helloHuman();
+        System.out.println();
+
+    }
+
+    private static void talk_3() {
+
+    Car granta = new Car("Lada", "Granta", 1.7d, "Желтый", 2015, "Россия");
+    Car audi = new Car("Audi", "A8", 50.0d, "", 2020, "Германия");
+    Car bmw = new Car("BMW", "Z8", 3.0d, "Черный", 2021, "Германии");
+    Car kia = new Car("Kia", "Sportage", 2.4d, "Красный", 2018, "");
+    Car hyundai = new Car("Hyundai", "Avante", 0, "Оранжевый", 0, "Южной Корее");
+
+    granta.getOptions();
+    audi.getOptions();
+    bmw.getOptions();
+    kia.getOptions();
+    hyundai.getOptions();
+    System.out.println();
+
+    }
+
+    private static void talk_4() {
+
+    }
+
 }
+
